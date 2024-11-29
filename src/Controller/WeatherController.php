f1c7991123862ee7da5b8f18730d477f516b588e
unknown <bouzire.noureddine1@gmail.com>
2024-11-29 16:01:15 +0100

    update the weather controller

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,14 +2,59 @@
 declare(strict_types =1);
 namespace App\Controller;
 
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WeatherController{
+class WeatherController extends AbstractController{
     #[Route("/weather/{countryCode}/{city}")]
     public function forecast(string $countryCode, string $city ): Response{
-        $content = "<html><body>$countryCode: $city </body></html>";
-        return new Response($content);
+        $forcasts = [
+        
+        [
+            "date" => new DateTime('2024-01-01'),
+            "temperatureCelsius" => 17,
+            "flTemperaturCelsuis" => 16,
+            "pressure" => 1000,
+            "humidity" => 64,
+            "wind_speed" => 3.2,
+            "wind_deg" => 270,
+            "cloudiness" => 75,
+            'icon' => 'sun'
+
+        ],
+        [
+            "date" => new DateTime('2024-01-02'),
+            "temperatureCelsius" => 17,
+            "flTemperaturCelsuis" => 16,
+            "pressure" => 1000,
+            "humidity" => 64,
+            "wind_speed" => 3.2,
+            "wind_deg" => 270,
+            "cloudiness" => 75,
+            'icon' => 'sun'
+        ],
+        [
+            "date" => new DateTime('2024-01-03'),
+            "temperatureCelsius" => 17,
+            "flTemperaturCelsuis" => 16,
+            "pressure" => 1000,
+            "humidity" => 64,
+            "wind_speed" => 3.2,
+            "wind_deg" => 270,
+            "cloudiness" => 75,
+            'icon' => 'sun'
+        ]
+        ];
+
+        $respnse = $this->render('weather/forcast.html.twig',[
+            'forcasts' => $forcasts,
+            'city' => $city,
+            'countryCode' => $countryCode
+        ]);
+        
+        return  $respnse;
 
     }
 }
